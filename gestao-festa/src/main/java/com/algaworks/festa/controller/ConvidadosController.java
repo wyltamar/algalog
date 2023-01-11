0c7a949c680680ba6af16495a99d7cb1f4dc870b
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2023-01-10 22:51:19 -0300

    Adicionando convidado

--- gestao-festa/src/main/java/com/algaworks/festa/controller/ConvidadosController.java
+++ gestao-festa/src/main/java/com/algaworks/festa/controller/ConvidadosController.java
@@ -3,26 +3,37 @@ package com.algaworks.festa.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.algaworks.festa.model.Convidado;
 import com.algaworks.festa.repository.ConvidadosRepository;
 
 @Controller
-@RequestMapping
+@RequestMapping(value ="/convidados")
 public class ConvidadosController {
 	
 	@Autowired
 	private ConvidadosRepository convidadosRepository;
 	
-	@GetMapping(value ="/convidados")
+	@GetMapping
 	public ModelAndView listar() {
 		
 		ModelAndView modelAndView = new ModelAndView("ListaConvidados");
 		
 		modelAndView.addObject("convidadosRepository", convidadosRepository.findAll());
+		modelAndView.addObject(new Convidado());
 		
 		return modelAndView;
+	}
+	
+	@PostMapping
+	public String salvar(Convidado convidado) {
+		
+		this.convidadosRepository.save(convidado);
+		
+		return "redirect:/convidados";
 	}
 
 }
