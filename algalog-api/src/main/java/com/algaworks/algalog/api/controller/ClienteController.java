a2261f14567a07f4fa053342c3ffe66be719ce48
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2023-01-22 15:12:18 -0300

    Recorrendo ao lombok para fazer a injeção de dependência através do método construtor

--- algalog-api/src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
+++ algalog-api/src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
@@ -9,16 +9,15 @@ import org.springframework.web.bind.annotation.RestController;
 import com.algaworks.algalog.domain.model.Cliente;
 import com.algaworks.algalog.domain.repository.ClienteRepository;
 
+import lombok.AllArgsConstructor;
+
 @RestController
+@AllArgsConstructor
 public class ClienteController {
 	
 	
 	private ClienteRepository clienteRepository;
 	
-	public ClienteController(ClienteRepository clienteRepository) {
-		super();
-		this.clienteRepository = clienteRepository;
-	}
 	
 	@GetMapping(value = "/clientes")
 	public List<Cliente> listar() {
