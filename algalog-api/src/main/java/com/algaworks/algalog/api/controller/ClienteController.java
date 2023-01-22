e5f6ca585ab76c236349094d3322a6a95d570442
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2023-01-22 09:02:24 -0300

    Utilizando EntityManager para gerenciar as entidades refletidas na base de dados

--- algalog-api/src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
+++ algalog-api/src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
@@ -1,7 +1,9 @@
 package com.algaworks.algalog.api.controller;
 
-import java.util.Arrays;
 import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,23 +13,14 @@ import com.algaworks.algalog.domain.model.Cliente;
 @RestController
 public class ClienteController {
 	
+	@PersistenceContext
+	private EntityManager manager;
+	
 	@GetMapping(value = "/clientes")
 	public List<Cliente> listar() {
 		
-		var cliente1 = new Cliente();
-		cliente1.setId(1L);
-		cliente1.setNome("Pedro Lucas");
-		cliente1.setEmail("pedro@gmail.com");
-		cliente1.setTelefone("83 9 99645517");
+		return manager.createQuery("from Cliente", Cliente.class).getResultList();
 		
-		var cliente2 = new Cliente();
-		cliente2.setId(2L);
-		cliente2.setNome("Pietra");
-		cliente2.setEmail("piedra@gmail.com");
-		cliente2.setTelefone("83 8 45645517");
-
-
-		return Arrays.asList(cliente1, cliente2);
 	}
 
 }
