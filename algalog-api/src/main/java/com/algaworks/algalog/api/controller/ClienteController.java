4de0cdd5b89dd20c212083ef5e644666d98fdcbd
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2023-01-22 15:10:52 -0300

    Injetando dependência através do método construtor

--- algalog-api/src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
+++ algalog-api/src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
@@ -2,24 +2,28 @@ package com.algaworks.algalog.api.controller;
 
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.algalog.domain.model.Cliente;
+import com.algaworks.algalog.domain.repository.ClienteRepository;
 
 @RestController
 public class ClienteController {
 	
-	@PersistenceContext
-	private EntityManager manager;
+	
+	private ClienteRepository clienteRepository;
+	
+	public ClienteController(ClienteRepository clienteRepository) {
+		super();
+		this.clienteRepository = clienteRepository;
+	}
 	
 	@GetMapping(value = "/clientes")
 	public List<Cliente> listar() {
 		
-		return manager.createQuery("from Cliente", Cliente.class).getResultList();
+		return clienteRepository.findAll();
 		
 	}
 
