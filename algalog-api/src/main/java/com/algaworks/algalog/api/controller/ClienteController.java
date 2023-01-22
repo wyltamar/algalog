7a6111a0a74e1a6bbed5c2599343d583e4c58e49
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2023-01-21 22:39:55 -0300

    Adicionando o conceito de variáveis

--- algalog-api/src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
+++ algalog-api/src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
@@ -14,13 +14,13 @@ public class ClienteController {
 	@GetMapping(value = "/clientes")
 	public List<Cliente> listar() {
 		
-		Cliente cliente1 = new Cliente();
+		var cliente1 = new Cliente();
 		cliente1.setId(1L);
 		cliente1.setNome("Pedro Lucas");
 		cliente1.setEmail("pedro@gmail.com");
 		cliente1.setTelefone("83 9 99645517");
 		
-		Cliente cliente2 = new Cliente();
+		var cliente2 = new Cliente();
 		cliente2.setId(2L);
 		cliente2.setNome("Pietra");
 		cliente2.setEmail("piedra@gmail.com");
